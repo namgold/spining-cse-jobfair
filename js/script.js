364f7024d4e1068282c6929a3fe18c7b8fc829cb
Nam Nguyen <hnamnguyencse@gmail.com>
2019-11-27 16:21:57 +0700

    .

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ function getSV() {
         "1713057 Trần Văn Tâm", "1814812 ahihi tớ nè", "1912463 PHẠM HOÀNG VŨ", "1710187 Nguyễn Thị Trúc Ly", "1712638 Huỳnh Quốc Phú", "1712516 Lê Thành Nhơn", "1911900 ĐINH GIA QUANG", "1712025 Nguyễn Việt Long", "1811867 Ngô Tấn ĐẠT", "1814149 Nguyễn Duy Thìn", "1711807 Đỗ Đăng Khôi", "1710195 Nguyễn Đăng Hà Nam", "1912123 LÊ TRẦN HOÀNG THỊNH", "1714075 Cao Ngọc Xuân Yến", "1911704 NGUYỄN THỦY NGỌC", "1912190 NGUYỄN MAI THY", "1911594 HOÀNG ĐĂNG MINH", "1911591 ĐỖ QUANG MINH", "1910339 LÊ HỮU ĐỨC MINH", "1910644 NGUYỄN HOÀNG TRUNG", "1910123 NGUYỄN PHÙNG HẢI ĐĂNG", "1910094 Trần Đức Duy", "1911565 CAO THỊ THANH MAI", "1710477 Nguyễn Công Anh", "1910006 Nguyễn Ngọc Thái An", "1910060 NHAN MINH CHÂU", "1713239 Nguyễn Hữu Thắng", "1711371 Phạm Phước Hoài", "1910137 NGUYỄN HUỲNH ĐỨC", "1713454 Trần Hữu Thức", "1711948 Lê Công Linh", "1712855 Phan Tấn Quốc", "1710780 Lê Anh Duy", "1814182 Phan Khánh Thịnh", "1712831 Trần Minh Quân", "1911314 LƯƠNG THỊ QUỲNH HƯƠNG", "1711754 Từ Nguyên Gia Khiêm", "1710738 Nguyễn Công Danh", "1710738 Nguyễn Công Danh", "1711861 Nguyễn Hàn Mạnh Kiệt", "1711790 Nguyễn Trần Phương Khoa", "1711355 Nguyễn Lê Hoàng Hiệu", "1712179 Nguyễn Nhật Minh", "1711651 Đàm Ngọc Hương", "1713417 Đặng Nguyễn Minh Thư", "1812516 NGUYỄN VĂN HỮU", "1713177 Lê Thị Thanh Thảo", "1812869 Hồ Thiên Long", "1713775 Đinh Đức Tuấn", "1914661 Nguyễn Quốc Phú", "1710259 Bùi Việt Minh Quân", "1911296 ĐẶNG PHƯỚC VĨNH HƯNG", "1710735 Vương Chí Cường", "1913446 Nguyễn Trần Hoàng", "1712817 Lê Bá Quân", "1710228 Nguyễn Ngọc Phát", "1814710 Trần Khánh Tùng", "1912835 Trần Quốc Cường", "1810657 Trần Hoàng Việt", "1710009 Phan Gia Anh", "1710853 Đặng Văn Dũng", "1711271 Danh Trí Hiếu", "1710849 Cao Đăng Dũng", "1714050 Nguyễn Khải Vy", "1713521 Nguyễn Trung Tính", "1810388 Nguyễn Thành NHÂN", "1713215 Bùi Thanh Thắng", "1710433 Nguyễn Thành Khánh An", "1711014 Nguyễn Trần Minh Đăng", "1711457 Trần Xuân Hội", "1712802 Trần Thanh Quang", "1813084 Nguyễn Hoàng Minh", "1711726 Bùi Quốc Khải", "1613001 Dương Văn Tài", "1711547 Nguyễn Xuân Huy", "1912526 Nguyễn Đình An", "1912594 Nguyễn Quang Anh", "1711020 Chung Minh Đệ", "1811015 Lê Phương Khuê", "1810987 Đỗ Việt Vân Khanh", "1812924 Nguyễn Thăng Long", "1814436 Lê Thanh TRIỀU", "1711835 Võ Đình Khương", "1710934 Châu Thành Đạt", "1713375 Đỗ Vương Thế Thục Anh", "1812883 LÊ HỮU VÕ LONG", "1814205 Nguyễn Phi Thông", "1814220 Nguyễn Văn THUẦN", "1713490 Phạm Hồng Tiến", "1612305 Đoàn Lê", "1712380 Nguyễn Duy Nguyên", "1713511 Công Huyền Tôn Nữ Ngọc Huyền", "1712964 Hồ Công Sơn", "1712928 Hoàng Văn Sang", "1713952 Phạm Hồng Việt", "1812694 PHẠM NGUYỄN THÁI KHƯƠNG", "1713153 Nguyễn Công Thành", "1814506 LÊ HOÀNG BẢO TRUNG", "1812426 Trần Quang Huy", "1612115 Nguyễn Hoàng Nam"
     ]
 }
+
 jQuery.extend( jQuery.easing, {
     easeOutElastic: function(x, t, b, c, d) {
         var ts=(t/=d)*t;
@@ -10,6 +11,7 @@ jQuery.extend( jQuery.easing, {
         return b+c*(33*tc*ts + -106*ts*ts + 126*tc + -67*ts + 15*t);
     }
 })
+
 $(document).ready(function() {
     var users = [],
         shuffled = [],
